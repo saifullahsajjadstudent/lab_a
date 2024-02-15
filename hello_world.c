fde0faab5e5451e710f6ea4fa89fd01466f91bd0
Saifullah Sajjad <saifullah.sajjad@student.manchester.ac.uk>
2024-02-15 16:18:57 +0000

    Test 2

--- hello_world.c
+++ hello_world.c
@@ -4,7 +4,7 @@ int main(void)   // function which is run when the programs starts
 
 { 
 
-printf("Hello. My email address is: saifullah.sajjad@student.manchester.ac.uk "); // our statement/command to run 
+printf("Hello. My email address is: saifullah.sajjad@student.manchester.ac.uk"); // our statement/command to run 
 
     return 0; // the return statement for the function main
 
