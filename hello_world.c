fcc5877e6041ca22fbecb6b6657fe29f691669d9
Saifullah Sajjad <saifullah.sajjad@student.manchester.ac.uk>
2024-02-15 16:11:31 +0000

    test

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // directive to include 
+
+int main(void)   // function which is run when the programs starts 
+
+{ 
+
+printf("Hello. My email address is: saifullah.sajjad@student.manchester.ac.uk "); // our statement/command to run 
+
+    return 0; // the return statement for the function main 
+
+} 
